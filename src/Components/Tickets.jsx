1de876aec7cb7eb5fea99e1fb0bfa38eb6f7b205
Chayapatr Archiwaranguprok <chayapatr.arc@gmail.com>
2022-06-13 17:45:11 +0700

    feat: new ticket types

--- src/Components/Tickets.jsx
+++ src/Components/Tickets.jsx
@@ -1,23 +1,38 @@
 import earlyBirdImg from "../assets/tickets/b0_earlybird.png"
 import stupidPitchImg from "../assets/tickets/b1_stupidpitch.png"
+import ctfImg from "../assets/tickets/b2_ctf.png"
+import treasureHunterImg from "../assets/tickets/b3_treasure_hunter.png"
+import stupidAdventureImg from "../assets/tickets/b4_stupid_adventure.png"
 import styles from "../App.module.scss"
 import classNames from "classnames"
 
 export function Tickets () {
   const ticketTypes = [
     {
-      name: "Early Bird",
-      href: "https://web.facebook.com/505764236423154/posts/pfbid02VLgLbiSm9VeBYfDG4ueYNK9pVDazLyDB1jDsUqpjUKAVN3dwkuLpJdkw9uqzugZkl",
+      name: "Batch[0] = Early Bird",
+      href: "https://www.facebook.com/StupidHackTH/posts/pfbid02VLgLbiSm9VeBYfDG4ueYNK9pVDazLyDB1jDsUqpjUKAVN3dwkuLpJdkw9uqzugZkl",
       imgSrc: earlyBirdImg,
     },
     {
-      name: "Stupid Pitch",
+      name: "Batch[1] = Stupid Pitch Challenge",
       href: "https://www.facebook.com/StupidHackTH/posts/pfbid033hnWHpJkqjjvQyynzUdfuU8Sq9hWfpY2VY8MdDxoWoJSpqjJJHVrxhcVFuXDLY2Gl",
       imgSrc: stupidPitchImg,
     },
-    {name: "Batch 2"},
-    {name: "Batch 3"},
-    {name: "Batch 4"},
+    { 
+      name: "Batch[2] = Beyond the Canvas, a CTF Challenge by SaltyAom",
+      href: "https://www.facebook.com/StupidHackTH/posts/pfbid0ABMxzwZKtSGdpvrsXV9d1Wy3aXz64wCCSobu7LbspRYBLSFgMxhjansPj8rn2v1Vl", 
+      imgSrc: ctfImg,
+    },
+    { 
+      name: "Batch[3] = Treasure Hunter by showdown.space",
+      href: "https://facebook.com/stupidhackth", 
+      imgSrc: treasureHunterImg,
+    },
+    { 
+      name: "Batch[4] = Stupid Adventure, a text game by Rayriffy",
+      href: "https://facebook.com/stupidhackth", 
+      imgSrc: stupidAdventureImg,
+    },
     {name: "Batch 5"}
   ]
   return (
